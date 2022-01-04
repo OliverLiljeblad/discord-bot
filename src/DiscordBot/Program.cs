cf75cf72792bf0fcb1212e32e2eddc979a932aad
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2022-01-04 18:14:32 +0100

    Discord bot with functions

--- src/DiscordBot/Program.cs
+++ src/DiscordBot/Program.cs
@@ -3,6 +3,10 @@ using Discord.WebSocket;
 
 Console.WriteLine("DiscordBot starting");
 
+int number = 0;
+
+bool HasGameStarted = false;
+
 var client = new DiscordSocketClient();
 client.Log += LogAsync;
 client.Ready += ReadyAsync;
@@ -36,6 +40,41 @@ async Task MessageReceivedAsync(SocketMessage message)
     if (message.Author.Id == client.CurrentUser.Id)
     return;
 
-    if (message.Content == "!ping")
-      await message.Channel.SendMessageAsync("pong!");
+    if (message.Content == "!help")
+        await message.Channel.SendMessageAsync("Commands: \n !help \n !play \n !stop \n !creator");
+
+    if (message.Content == "!creator")
+    {
+        await message.Channel.SendMessageAsync("Oliver#2191");
+    }
+
+    if (message.Content == "!play") 
+    {
+        await message.Channel.SendMessageAsync("Shall we play a game? \n I think of a number between 1-100");
+        HasGameStarted = true; 
+        Random rnd = new Random();
+        number = rnd.Next(0, 101);
+        return;
+    }
+
+    if (message.Content == "!stop")
+        HasGameStarted = false;
+
+if(HasGameStarted == true) {
+    if (int.Parse(message.Content) > number && HasGameStarted == true) 
+        await message.Channel.SendMessageAsync("Lower");
+    
+    if (int.Parse(message.Content) < number && HasGameStarted == true)
+        await message.Channel.SendMessageAsync("Higher");
+
+    if (int.Parse(message.Content) < 0 || int.Parse(message.Content) > 100 && HasGameStarted == true)
+        await message.Channel.SendMessageAsync("The number is between 1 - 100");
+
+    if (int.Parse(message.Content) == number && HasGameStarted == true) 
+    {
+        await message.Channel.SendMessageAsync("Correct, well played!");
+        HasGameStarted = false;
+        return;
+    }
+ }
 }
