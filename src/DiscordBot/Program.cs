a4e74414bbd7ba7be16a199f8b8551c87b00f0e9
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2022-01-04 15:50:41 +0100

    Ping pong bot

--- src/DiscordBot/Program.cs
+++ src/DiscordBot/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+Console.WriteLine("DiscordBot starting");
+
+var client = new DiscordSocketClient();
+client.Log += LogAsync;
+client.Ready += ReadyAsync;
+client.MessageReceived += MessageReceivedAsync;
+
+var token = Environment.GetEnvironmentVariable("Discord-Bot-Token");
+Console.WriteLine($"Token: {token}");
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+await Task.Delay(Timeout.Infinite);
+
+Task LogAsync(LogMessage log)
+{
+    Console.WriteLine("Nu loggar jag");
+    Console.WriteLine(log.ToString());
+    return Task.CompletedTask;
+}
+
+Task ReadyAsync()
+{
+    Console.WriteLine($"{client.CurrentUser} is connected!");
+    
+    return Task.CompletedTask;
+}
+
+async Task MessageReceivedAsync(SocketMessage message)
+{
+    Console.WriteLine(message.Content);
+
+    if (message.Author.Id == client.CurrentUser.Id)
+    return;
+
+    if (message.Content == "!ping")
+      await message.Channel.SendMessageAsync("pong!");
+}
